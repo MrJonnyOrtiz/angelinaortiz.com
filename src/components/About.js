6c7445622975694b37a74153636330e716679c03
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2023-11-06 16:55:32 -0500

    reformatted about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,45 +1,78 @@
+import gPortrait from "../media/gPortrait.jpeg";
 import candid from "../media/gAtMarket.jpeg";
+import gAtSCF from "../media/gAtSCF.jpeg";
 import fb from "../media/fb.png";
 import insta from "../media/insta.png";
 
 export default function About({ onTab }) {
    return (
-      <div id="about" className="text-center px-5 py-2 lg:px-32">
-         <h2 className="text-2xl font-bold"> Hi! </h2>
-         <p className="p-2">
-            I'm Angelina and I'm so excited you visited my website! I have a
-            passion for music, performing with Juliet - my cello - for&nbsp;
-            {new Date().getFullYear() - 2015} years, and singing too! I enjoy
-            spreading love through music.&nbsp;
-            <a
-               href="#contact"
-               onClick={() => onTab(3)}
-               className="hover:bg-black hover:text-white"
-            >
-               Contact
-            </a>{" "}
-            me if you have any questions. Visit me on my social media. I hope to
-            hear from you soon!
-         </p>
-         <div className="flex flex-row justify-center mb-3">
-            <a
-               href="https://www.facebook.com/angelwingsortiz/"
-               target="_blank"
-               rel="noreferrer"
-            >
-               <img src={fb} alt="" />
-            </a>
-            <a
-               href="https://www.instagram.com/angelinalacherie/"
-               target="_blank"
-               rel="noreferrer"
-            >
-               <img src={insta} alt="" />
-            </a>
-         </div>
-
-         <div className="relative left-1/2 -translate-x-1/2 md:w-1/2">
-            <img src={candid} alt="Angelina playing at a farmer's market" />
+      <div id="about" className="container">
+         <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-[300px_1fr)] gap-4">
+            <div className="md:col-span-2 p-3">
+               <h2 className="text-2xl text-center font-bold col-span-2 md:py-3">
+                  Hello and welcome!
+               </h2>
+            </div>
+            <div className="md:row-span-2 md:m-5 lg:m-10 xl:m-12">
+               <div className="md:text-xl text-center md:font-bold md:py-3">
+                  <h3>
+                     <p className="md:pb-5">
+                        I'm Angelina and I'm so excited you visited my website!
+                     </p>
+                     <p className="md:pb-5">
+                        I have a passion for music, performing with{" "}
+                        <span className="italic">Juliet</span> - my cello -
+                        for&nbsp;
+                        {new Date().getFullYear() - 2015} years, and singing
+                        too!
+                     </p>
+                     <p className="md:pb-5">
+                        I enjoy spreading love through music.
+                     </p>
+                  </h3>
+               </div>
+               <div className="md:text-xl text-center md:font-bold md:pb-5">
+                  <h3>
+                     <a
+                        href="#contact"
+                        onClick={() => onTab(3)}
+                        className="hover:underline"
+                     >
+                        Contact
+                     </a>{" "}
+                     me if you have any questions. Visit me on my social media.
+                     I hope to hear from you soon!
+                  </h3>
+               </div>
+               <div className="flex flex-row justify-center mb-3">
+                  <a
+                     href="https://www.facebook.com/angelwingsortiz/"
+                     target="_blank"
+                     rel="noreferrer"
+                  >
+                     <img src={fb} alt="" />
+                  </a>
+                  <a
+                     href="https://www.instagram.com/angelinalacherie/"
+                     target="_blank"
+                     rel="noreferrer"
+                  >
+                     <img src={insta} alt="" />
+                  </a>
+               </div>
+            </div>
+            <div className="relative left-1/2 -translate-x-1/2 md:w-1/2 py-3">
+               <img
+                  className=""
+                  src={candid}
+                  alt="Angelina playing at a farmer's market"
+               />
+               {/* <img
+                  className="hidden md:block absolute -top-8 -right-12 w-1/2 "
+                  src={gAtSCF}
+                  alt="Angelina playing at a SCF"
+               /> */}
+            </div>
          </div>
       </div>
    );
