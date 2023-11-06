76c2c4ec1630df07d03f973b7ec9c34fbef8379f
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2023-11-06 17:20:35 -0500

    reformatted media items into 2 cols

--- src/components/Media.js
+++ src/components/Media.js
@@ -33,7 +33,7 @@ export default function Media() {
          <h2 className="text-xl mb-5 font-semibold">
             Juliet and I love to perform.
          </h2>
-         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 ">
+         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
             {media.map((media) => (
                <div key={media.id} className="place-self-center ">
                   <p className="text-sm">{media.title}</p>
